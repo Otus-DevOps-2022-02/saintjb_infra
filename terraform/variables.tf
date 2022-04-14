8173e0e6dd31cb0f61b9a88450502b70bb065fa8
saintjb <vikuvsh95@gmail.com>
2022-04-14 19:14:43 +0500

    terraform branch

---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable cloud_id {
+  description = "Cloud"
+}
+variable folder_id {
+  description = "Folder"
+}
+variable zone {
+  description = "Zone"
+  default = "ru-central1-a"
+}
+variable public_key_path {
+  description = "Public key path"
+}
+variable private_key_path {
+  description = "Private key path"
+}
+variable region_id {
+  description = "region id"
+  default = "ru-central-1"
+}
+variable disk_img {
+  description = "Image of disk"
+}
+variable service_acc_key {
+  description = "key.json"
+}
+
+variable subnet_id {
+  description = "subnet id"
+}
