eb43a08b56a9161a2f8d22d5a4dec1defa84f190
saintjb <vikuvsh95@gmail.com>
2022-04-17 14:21:37 +0500

    changes

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
-sudo apt-get install apt-transport-https ca-certificates -y
-wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add - 
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update -y
-sudo apt install -y mongodb-org
+#!/bin/bash
+
+sudo apt-get --assume-yes install apt-transport-https ca-certificates
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get --assume-yes update
+sudo apt-get --assume-yes upgrade
+sudo apt-get --assume-yes install mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
