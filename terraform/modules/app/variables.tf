298be82d027286a28cde78e36a200e8f4b2b5a13
saintjb <vikuvsh95@gmail.com>
2022-04-17 14:12:30 +0500

    add terraform 2

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,11 @@
+variable public_key_path {
+  description = "Public key"
+}
+ 
+variable subnet_id {
+  description = "subnet"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+}
---
+++ terraform/stage/variables.tf
@@ -0,0 +1,39 @@
+variable cloud_id {
+  description = "bngl1irg3c3h3u0qiofg"
+}
+variable folder_id {
+  description = "bngndlav8ernrfljukjc"
+}
+variable zone {
+  description = "Zone"
+  default = "ru-central1-a"
+}
+variable public_key_path {
+  description = "Public key"
+}
+variable private_key_path {
+  description = "Priv key"
+}
+variable region_id {
+  description = "region id"
+  default = "ru-central-1"
+}
+variable disk_img {
+  description = "Image of disk"
+}
+variable service_acc_key {
+  description = "key.json"
+  default = "key.json"
+}
+
+variable subnet_id {
+  description = "subnet id"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+}
